d192dc9e475fb0141533a657600d3c496e431608
John Olafenwa <johnolafenwa@gmail.com>
2019-06-07 18:39:30 +0000

    modified initializers

--- torchfusion_utils/initializers/initializers.py
+++ torchfusion_utils/initializers/initializers.py
@@ -13,7 +13,7 @@ def init(module,types,initializer,init_params=None,category="all"):
 
                         initializer(param.data)
                     else:
-                        initializer(param.data,*init_params)
+                        initializer(param.data,**init_params)
             
             elif category == "weight":
                 if hasattr(m,"weight"):
@@ -21,7 +21,7 @@ def init(module,types,initializer,init_params=None,category="all"):
 
                         initializer(m.weight.data)
                     else:
-                        initializer(m.weight.data,*init_params)
+                        initializer(m.weight.data,**init_params)
             elif category == "bias":
                 if hasattr(m,"bias"):
                     if hasattr(m.bias,"data"):
@@ -29,30 +29,30 @@ def init(module,types,initializer,init_params=None,category="all"):
 
                             initializer(m.bias.data)
                         else:
-                            initializer(m.bias.data,*init_params)
+                            initializer(m.bias.data,**init_params)
 
     module.apply(init_func)
 
 
 def normal_init(module,mean=0,std=1,types=[],category="all"):
 
-    return init(module,types,normal_,(mean,std),category)
+    return init(module,types,normal_,{"mean":mean,"std":std),category)
 
 def uniform_init(module,lower=0,upper=1,types=[],category="all"):
 
-    return init(module,types,uniform_,(lower,upper),category)
+    return init(module,types,uniform_,{"lower":lower,"upper":upper},category)
 
 def constant_init(module,value,types=[],category="all"):
 
-    return init(module,types,constant_,value,category)
+    return init(module,types,constant_,{"val":value},category)
 
 def ones_init(module,types=[],category="all"):
 
-    return init(module,types,constant_,(1,),category)
+    return init(module,types,constant_,{"val":1},category)
 
 def zeros_init(module,types=[],category="all"):
 
-    return init(module,types,constant_,(0,),category)
+    return init(module,types,constant_,{"val":0},category)
 
 def eye_init(module,types=[],category="all"):
 
@@ -65,24 +65,24 @@ def dirac_init(module,types=[],category="all"):
 
 def sparsity_init(module,sparsity_ratio=0.1,std=0.01,types=[],category="all"):
 
-    return init(module,types,sparse_,(sparsity_ratio,std),category)
+    return init(module,types,sparse_,{"sparsity":sparsity_ratio,"std":std},category)
 
 def kaiming_normal_init(module,neg_slope=0,mode="fan_in",non_linearity="leaky_relu",types=[],category="weight"):
 
-    return init(module,types,kaiming_normal_,(neg_slope,mode, non_linearity),category)
+    return init(module,types,kaiming_normal_,{"a":neg_slope,"mode":mode,"nonlinearity":non_linearity},category)
 
 def kaiming_uniform_init(module,neg_slope=0,mode="fan_in",non_linearity="leaky_relu",types=[],category="weight"):
 
-    return init(module,types,kaiming_uniform_,(neg_slope,mode, non_linearity),category)
+    return init(module,types,kaiming_uniform_,{"a":neg_slope,"mode":mode,"nonlinearity":non_linearity},category)
 
 def xavier_normal_init(module,gain=1,types=[],category="weight"):
 
-    return init(module,types,xavier_normal_,(gain,),category)
+    return init(module,types,xavier_normal_,{"gain":gain},category)
 
 def xavier_uniform_init(module,gain=1,types=[],category="weight"):
 
-    return init(module,types,xavier_uniform_,(gain,),category)
+    return init(module,types,xavier_uniform_,{"gain":gain},category)
 
 def orthorgonal_init(module,gain=1,types=[],category="all"):
 
-    return init(module,types,orthogonal_,(gain,),category)
+    return init(module,types,orthogonal_,{"gain":gain},category)
