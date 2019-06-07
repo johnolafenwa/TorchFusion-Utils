121dbc0e2b5bad16d167927c9acc74ed3624cf77
John Olafenwa <johnolafenwa@gmail.com>
2019-06-07 18:48:01 +0000

    v 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name="torchfusion-utils",
-      version='0.1.1',
+      version='0.1.2',
       description='A pytorch helper library for Mixed Precision Training, Initialization, Metrics and More Utilities to simplify training of deep learning models',
       url="https://github.com/johnolafenwa/TorchFusion-Utils",
       author='John Olafenwa and Moses Olafenwa (DeepQuest AI)',
