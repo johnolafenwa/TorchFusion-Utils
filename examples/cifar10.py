0bdae88b4019b29948d6cfa994ea88977e1f2d3c
John Olafenwa <johnolafenwa@gmail.com>
2019-06-08 10:35:20 +0000

    Update cifar10.py

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -1,96 +1,18 @@
-import torch
-import torch.nn as nn
-from torchfusion_utils.fp16 import convertToFP16
+from torchfusion_utils.fp16 import convertToFP16,convertToFP32
 from torchfusion_utils.initializers import *
 from torchfusion_utils.metrics import Accuracy
 from torchfusion_utils.models import load_model,save_model
+
+import torch
+import torch.nn as nn
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
 from torchvision.datasets import CIFAR10
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from tqdm import tqdm
-
-
-
-CUDA = False
-
-if torch.cuda.is_available():
-    CUDA = True
 
 BATCH_SIZE = 256
-NUM_EPOCHS = 100
-
-class ConvBlock(nn.Module):
-    def __init__(self,in_channels,out_channels):
-        super(ConvBlock,self).__init__()
-
-        self.net = nn.Sequential(
-            nn.Conv2d(in_channels,out_channels,kernel_size=3,padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
-    
-    def forward(self,x):
-
-        return self.net(x)
-
-
-class CifarNet(nn.Module):
-    def __init__(self,num_classes=10):
-        super(CifarNet,self).__init__()
-
-        self.net = nn.Sequential(
-
-            ConvBlock(3,32),
-            ConvBlock(32,32),
-            ConvBlock(32,32),
-
-            nn.MaxPool2d(3,2),
-
-            ConvBlock(32,64),
-            ConvBlock(64,64),
-            ConvBlock(64,64),
-
-            nn.MaxPool2d(3,2),
-
-            ConvBlock(64,128),
-            ConvBlock(128,128),
-            ConvBlock(128,128),
-
-            nn.AdaptiveAvgPool2d((1,1)),
-        )
-
-        self.classifier = nn.Linear(128,num_classes)
-
-    def forward(self,x):
-
-        x = self.net(x)
-        x = x.view(x.size(0),-1)
-        return self.classifier(x)
-
-model = CifarNet()
-
-if CUDA:
-    model = model.cuda()
-
-#initialize the weights
-kaiming_normal_init(model,types=[nn.Conv2d])
-xavier_normal_init(model,types=[nn.Linear])
-
-#initialize the bias
-zeros_init(model,category="bias")
-
-#setup optimizer
-
-optimizer = Adam(model.parameters())
-
-#convert to mixed precision mode
-
-model,optimizer = convertToFP16(model,optimizer)
-
-#create your lr scheduler
-lr_scheduler = StepLR(optimizer,step_size=30,gamma=0.1)
+NUM_EPOCHS = 200
 
 #create data transforms
 train_transforms = transforms.Compose([
@@ -117,59 +39,121 @@ test_dataset = CIFAR10(root="./cifar10",train=False,transform=test_transforms,do
 train_loader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True)
 test_loader = DataLoader(test_dataset,batch_size=BATCH_SIZE)
 
+class ConvBlock(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(ConvBlock,self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels,out_channels,kernel_size=3,padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+    
+    def forward(self,x):
+
+        return self.net(x)
+        
+class CifarNet(nn.Module):
+    def __init__(self,num_classes=10):
+        super(CifarNet,self).__init__()
+
+        self.net = nn.Sequential(
+
+            ConvBlock(3,64),
+            ConvBlock(64,64),
+            ConvBlock(64,64),
+
+            nn.MaxPool2d(3,2),
+
+            ConvBlock(64,128),
+            ConvBlock(128,128),
+            ConvBlock(128,128),
+
+            nn.MaxPool2d(3,2),
+
+            ConvBlock(128,128),
+            ConvBlock(128,128),
+            ConvBlock(128,128),
+            nn.Dropout(0.25),
+
+            nn.AdaptiveAvgPool2d((1,1)),
+        )
+
+        self.classifier = nn.Linear(128,num_classes)
+
+    def forward(self,x):
+
+        x = self.net(x)
+        x = x.view(x.size(0),-1)
+        return self.classifier(x)
+    
+model = CifarNet().cuda()
+
+#initialize the weights
+kaiming_normal_init(model,types=[nn.Conv2d])
+xavier_normal_init(model,types=[nn.Linear])
+
+#initialize batchnorm
+ones_init(model,types=[nn.BatchNorm2d],category="weight")
+zeros_init(model,types=[nn.BatchNorm2d],category="bias")
+
+#initialize all bias
+zeros_init(model,category="bias")
+
+optimizer = Adam(model.parameters())
+
+#convert to mixed precision mode
+model,optimizer = convertToFP16(model,optimizer)
+
+#create your lr scheduler
+lr_scheduler = StepLR(optimizer,step_size=30,gamma=0.1)
+
 
 #evaluation function
 def evaluation_loop():
 
     model.eval()
-
-    test_acc = Accuracy()
+    test_acc = Accuracy(topK=1)
 
     for i,(x,y) in enumerate(test_loader):
-        
         x = x.cuda()
         y = y.cuda()
-
         predictions = model(x)
-
         test_acc.update(predictions,y)
+        
     return test_acc.getValue()
+
 
 #training function
 def train_loop():
 
     loss_fn = nn.CrossEntropyLoss()
-
     train_acc = Accuracy()
-
     best_acc = 0
 
     for e in range(NUM_EPOCHS):
 
         model.train()
-
+        
+        #reset the acc on every epoch
         train_acc.reset()
-
         total_loss = 0
 
-        for i,(x,y) in tqdm(enumerate(train_loader)):
-
+        for i,(x,y) in enumerate(train_loader):
+        
             optimizer.zero_grad()
-
-            if CUDA:
-                x = x.cuda()
-                y = y.cuda()
+            x = x.cuda()
+            y = y.cuda()
 
             predictions = model(x)
             loss = loss_fn(predictions,y)
 
             #replace loss.backward() with optimizer.backward(loss)
             optimizer.backward(loss)
-            
             optimizer.step()
 
             total_loss = total_loss + (loss.item() * x.size(0))
-
+            #update the acc
             train_acc.update(predictions,y)
 
         total_loss = total_loss/len(train_loader.dataset)
@@ -180,18 +164,7 @@ def train_loop():
 
         if test_acc > best_acc:
             best_acc = test_acc
-            save_model(model,"model_{}.pth".format(e))
+            full_model = convertToFP32(model)
+            save_model(full_model,"model_{}.pth".format(e))
     
         print("Epoch: {} Train Acc: {} Test Acc: {} Best Test Acc: {} Loss: {}".format(e,train_acc.getValue(),test_acc,best_acc,total_loss))
-
-
-train_loop()
-
-
-
-
-
-
-
-
-
